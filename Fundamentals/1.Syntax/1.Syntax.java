8a35c6e41da6af3135e6a0671d4c101e23048869
ShubhamCodeByte <shubhamsingh22401@gmail.com>
2026-04-06 06:31:12 +0530

    fumdamentals till data types

---
+++ Fundamentals/1.Syntax/1.Syntax.java
@@ -0,0 +1,10 @@
+
+// this is the suntax example print hello world
+
+class Test {
+  public static void main(String[] args) { // i have changed the name of the main to Main so error Error: Could not find
+                                           // or load main class .\Test.class
+
+    System.out.println("Hello world!");
+  }
+}
---
+++ Fundamentals/2.DataTypes/datatypes.java
@@ -0,0 +1,19 @@
+// this will contain all the data types
+
+
+// lets make a class first
+
+class DataTypes {
+  public static void main(){
+
+    // integer
+    int a = 1;
+    byte b = 1;
+    short c = 1;
+    long d = 1;
+
+    System.out.println(a + b + c + d);
+    
+
+  }
+}
